1a2916f3312a796f315a4b537d02d6e0c7a01470
Edwin Tobias Ariza Tellez <edwint0823@gmail.com>
2024-03-20 09:13:59 -0500

    app listen adjust

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.APP_PORT || 3000;
-  console.log(`listening on ${port}`);
 
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalFilters(new QueryExceptionFilter());
@@ -32,7 +31,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0', () => {
+    console.log(`listening on ${port}`);
+  });
 }
 
 bootstrap();
