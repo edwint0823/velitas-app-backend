4ceb6347b5a15ebe59084763603bb7ee647342c1
Edwin Tobias Ariza Tellez <edwint0823@gmail.com>
2024-03-28 11:25:08 -0500

    daysjs locale changed

--- core/constants.ts
+++ core/constants.ts
@@ -1,7 +1,7 @@
 export const priceTypeOptions: Array<string> = ['detal', 'mayorista'];
 export const minimumSizeBulkPriceNameParam: string = 'minimum_size_bulk_price';
 export const defaultMinimumSizeBulkPrice: number = 10;
-export const timeZoneDayjs: string = 'es';
+export const timeZoneDayjs: string = 'es-mx';
 export const timeToDoOneCandleNameParam: string = 'time_to_do_one_candle';
 export const minimumDaysToDoOrder: number = 2;
 export const orderCreateNameStatus: string = 'Creado';
