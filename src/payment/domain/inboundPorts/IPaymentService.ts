6a73c33337eb7ddd7c4f80b2a9a8e5fde343e7ec
Edwin Tobias Ariza Tellez <edwint0823@gmail.com>
2024-04-12 08:17:02 -0500

    delete todo

--- src/payment/domain/inboundPorts/IPaymentService.ts
+++ src/payment/domain/inboundPorts/IPaymentService.ts
@@ -1,6 +1,5 @@
 import { ListPaymentsByOrderDomain } from '../model/out/ListPaymentsByOrderDomain';
 
 export interface IPaymentService {
-  // todo implementar tipado
   getPaymentsByOrderCode(orderCode: string): Promise<ListPaymentsByOrderDomain[]>;
 }
